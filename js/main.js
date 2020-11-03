0faee4657ed48063a3f85834c35dd8e4391144e3
KataevAS <kataevjob@mail.ru>
2020-11-03 20:01:23 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -294,6 +294,54 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
 
+    //Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+
+    total.textContent = getZero(slides.length);
+    current.textContent = getZero(slideIndex);
+
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        current.textContent = getZero(slideIndex);
+
+        slides.forEach(slide => {
+            slide.style.display = 'none';
+        });
+
+        slides[slideIndex - 1].style.display = 'block';
+    }
+
+    function changeSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        changeSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        changeSlides(1);
+    });
+
+
+
 
 
 
