bc13c6a227406219d904e9372c8322fe8d1ff958
KataevAS <kataevjob@mail.ru>
2020-11-03 19:23:50 +0300

    add axios library

--- js/main.js
+++ js/main.js
@@ -197,32 +197,13 @@ window.addEventListener('DOMContentLoaded', () => {
     //         });
     //     });
 
-    getResource('http://localhost:3000/menu')
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            createCard(data);
+            data.data.forEach(({ img, altimg, title, descr, price }) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+
         });
-
-    function createCard(data) {
-        data.forEach(({ img, altimg, title, descr, price }) => {
-            const element = document.createElement('div');
-            element.classList.add('menu__item');
-            element.innerHTML = `
-                <img src=${img} alt=${altimg}>
-                <h3 class="menu__item-subtitle">${title}</h3>
-                <div class="menu__item-descr">
-                    ${descr}
-                </div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                     <div class="menu__item-cost">Цена:</div>
-                     <div class="menu__item-total"><span>${price}</span> грн/день</div>
-                </div>
-            `;
-
-            document.querySelector('.menu .container').append(element);
-        });
-    }
-
 
     //--------------Forms--------------
 
